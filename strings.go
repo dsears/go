6f4f1127f64070690f1ed6fd6d91c8c3b044f995
Drew Sears <dsears@gmail.com>
2019-11-10 09:12:04 -0500

    resolves #1

---
+++ strings.go
@@ -0,0 +1,54 @@
+package dsears
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
+
+var reAlphaNumeric *regexp.Regexp
+
+func init() {
+	var err error
+	reAlphaNumeric, err = regexp.Compile("[^A-za-z0-9]+")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// ToFuzzyString : Return a normalized version of a string suitable for fuzzy matching
+func ToFuzzyString(s string) string {
+	s = removeAccents(s)
+	s = to7Bit(s)
+	s = strings.ToLower(s)
+	s = reAlphaNumeric.ReplaceAllString(s, "")
+	return s
+}
+
+func removeAccents(s string) string {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	output, _, err := transform.String(t, s)
+	if err != nil {
+		panic(err)
+	}
+	return output
+}
+
+// Convert to 7 bit ASCII by replacing high codepoint characters with rune numbers
+func to7Bit(s string) string {
+	r := []rune(s)
+	a := make([]string, len(r))
+	for i, v := range r {
+		if v > 127 {
+			a[i] = fmt.Sprintf("x%d", v)
+		} else {
+			a[i] = string(v)
+		}
+	}
+	return strings.Join(a, "")
+}
---
+++ strings_test.go
@@ -0,0 +1,22 @@
+package dsears
+
+import "testing"
+
+func TestFuzzyStrings(t *testing.T) {
+	tables := []struct {
+		input    string
+		expected string
+	}{
+		{"War & Peace", "warpeace"},
+		{"Secret of N.I.M.H.", "secretofnimh"},
+		{"Amélie", "amelie"},
+		{"8½ (1963)", "8x1891963"},
+		{"banana", "banana"},
+	}
+	for _, table := range tables {
+		actual := ToFuzzyString(table.input)
+		if actual != table.expected {
+			t.Errorf("Expected %s, got %s", table.expected, actual)
+		}
+	}
+}
